d5f6af9c39d5ef0a6a9b42ba0b8de30d59e191c2
nikdalvi <62291136+nikdalvi@users.noreply.github.com>
2020-05-04 20:40:46 +0530

    Update code.py

--- Loan-appoval-analysis-using-Matplotlib/code.py
+++ Loan-appoval-analysis-using-Matplotlib/code.py
@@ -1,93 +1,72 @@
-# --------------
 #Importing header files
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv(path)
+
+#Reading the file
+data=pd.read_csv(path)
 
 #Code starts here
-loan_status = data['Loan_Status'].value_counts()
 
+# Step 1 
+#Reading the file
+
+
+#Creating a new variable to store the value counts
+loan_status = data['Loan_Status'].value_counts()
+#Plotting bar plot
 plt.bar(loan_status.index,loan_status)
 
-
-# --------------
-#Code starts here
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
+# Step 2
+#Plotting an unstacked bar plot
 property_and_loan = data.groupby(['Property_Area','Loan_Status']).size().unstack()
-
-property_and_loan.plot(kind='bar', stacked=False, figsize=(10,5))
-
+property_and_loan.plot(kind='bar',stacked=False,figsize=(10,5))
+#Changing the x-axis label
 plt.xlabel('Property Area')
+#Changing the y-axis label
 plt.ylabel('Loan Status')
-plt.xticks(rotation=45)
-
-
-
-
-
-# --------------
-#Code starts here
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-education_and_loan = data.groupby(['Education','Loan_Status']).size().unstack()
-
-education_and_loan.plot(kind='bar',stacked=True, figsize=(10,5))
-plt.xlabel('Education Status')
-plt.ylabel('Loan Status')
+#Rotating the ticks of X-axis
 plt.xticks(rotation = 45)
 
+# Step 3
+#Plotting a stacked bar plot
+education_and_loan = data.groupby(['Education','Loan_Status']).size().unstack()
+education_and_loan.plot(kind='bar',stacked=True,figsize=(10,5))
+#Changing the x-axis label
+plt.xlabel('Education Status')
+#Changing the y-axis label
+plt.ylabel('Loan Status')
+#Rotating the ticks of X-axis
+plt.xticks(rotation=45)
 
-
-
-
-
-# --------------
-#Code starts here
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
+# Step 4 
+#Subsetting the dataframe based on 'Education' column
 graduate = data[data['Education']=='Graduate']
+#Subsetting the dataframe based on 'Education' column
 not_graduate = data[data['Education']=='Not Graduate']
-
-graduate['LoanAmount'].plot(kind='density' ,label='Graduate')
-not_graduate['LoanAmount'].plot(kind='density', label='Not Graduate')
-
-#Code ends here
-
+#Plotting density plot for 'Graduate'
+graduate.plot(kind='density',label='Graduate')
+#Plotting density plot for 'Not Graduate'
+not_graduate.plot(kind='density',label='Not Graduate')
 #For automatic legend display
 plt.legend()
 
+# Step 5
+#Setting up the subplots
+fig,(ax_1,ax_2,ax_3) = plt.subplots(nrows=3,ncols=1, figsize=(10,5))
 
-# --------------
-#Code starts here
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-fig,(ax_1,ax_2,ax_3) = plt.subplots(nrows = 3 , ncols = 1,figsize=(10,5))
-
-data.plot.scatter(x='ApplicantIncome', y='LoanAmount')
+#Plotting scatter plot
+data.plot.scatter(x='ApplicantIncome',y='LoanAmount')
+#Setting the subplot axis title
 ax_1.set_title('Applicant Income')
-
-data.plot.scatter(x='CoapplicantIncome', y='LoanAmount')
+#Plotting scatter plot
+data.plot.scatter(x='CoapplicantIncome',y='LoanAmount')
+#Setting the subplot axis title
 ax_2.set_title('Coapplicant Income')
-
-#New Column
-data['TotalIncome'] = data['ApplicantIncome'] + data['CoapplicantIncome']
-
-#Axis 3
-data.plot.scatter(x='TotalIncome', y='LoanAmount')
+#Creating a new column 'TotalIncome'
+data['TotalIncome']=data['ApplicantIncome']+data['CoapplicantIncome']
+#Plotting scatter plot
+data.plot.scatter(x='TotalIncome',y='LoanAmount')
+#Setting the subplot axis title
 ax_3.set_title('Total Income')
-
-
-
-
-
